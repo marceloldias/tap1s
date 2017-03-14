7925900ec034e8ecc070acfebf747bdd9beb17a3
marceloldias <marcelodias94@gmail.com>
2017-03-13 22:09:18 -0400

    update delete

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -16,9 +16,9 @@ import model.Usuario;
 public class UsuarioController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private List<Usuario> lista = new ArrayList<>();
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// capturando o que vem do client
@@ -26,31 +26,71 @@ public class UsuarioController extends HttpServlet {
 		String email = req.getParameter("email");
 
 		Usuario usu = new Usuario(nome, email);
-		
-		//inserir na lista
+
+		// inserir na lista
 		lista.add(usu);
-		
-		
-		resp.getWriter().print("\n{Nome: "+usu.getNome()+" email: "+usu.getEmail()+" }");
+
+		resp.getWriter().print("\n{Nome: " + usu.getNome() + " email: " + usu.getEmail() + " }");
 		// Respondendo ao client
 		// resp.getWriter().print("\nOla "+ nome+" email: "+ email);
 
 	}
 
 	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		int ind = Integer.parseInt(req.getParameter("ind"));
+		
+		String nome = req.getParameter("nome");
+		String email = req.getParameter("email");	
+		
+				if(nome != null){
+					lista.get(ind).setNome(nome);
+				}
+				if(email != null){
+					lista.get(ind).setEmail(email);
+				}
+				
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int ind = Integer.parseInt(req.getParameter("ind"));	
+			
+				lista.remove(ind);
+			
+
+	}
+
+	//// @Override
+	//// protected void doDelete(HttpServletRequest req, HttpServletResponse
+	//// resp) throws ServletException, IOException {
+	////
+	//// String nome = req.getParameter("nome");
+	////
+	//// for(int i = 0; i<lista.size();i++){
+	//// if(lista.get(i).getNome().equals(nome)){
+	//// lista.remove(i);
+	//// i=-1;
+	//// }
+	//// }
+	//
+	// }
+
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		String json="[";
-		for(int i=0; i<lista.size();i++){
-			json+="\n{Nome: "+lista.get(i).getNome()+" ,email: "+lista.get(i).getEmail()+" }";
-			
-			if(i<lista.size()-1)
-				json+=",";
-			
+
+		String json = "[";
+		for (int i = 0; i < lista.size(); i++) {
+			json += "\n{Nome: " + lista.get(i).getNome() + " ,email: " + lista.get(i).getEmail() + " }";
+
+			if (i < lista.size() - 1)
+				json += ",";
+
 		}
-		json+="\n]";
-		
-		resp.getWriter().print(""+json);
+		json += "\n]";
+
+		resp.getWriter().print("" + json);
 	}
 
 }
